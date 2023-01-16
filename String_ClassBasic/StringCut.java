f6cdd32a16ffd19fcea14a9a650b52c528f99678
halinchanok <116154769+halinchanok@users.noreply.github.com>
2023-01-16 21:07:44 +0700

    Finish

--- String_ClassBasic/StringCut.java
+++ String_ClassBasic/StringCut.java
@@ -7,13 +7,20 @@ public class StringCut {
         Scanner sc =new Scanner(System.in);
         String str = sc.nextLine();
         String C = sc.next();  
+        boolean ck = false;
         for(int i=0; i<str.length(); i++){
-            char t = str.charAt(i);
-            if(C.equals(t)){
-                System.out.print(str.charAt(i));
-            }else{
+            if(ck){
                 System.out.println();
             }
+            char t = str.charAt(i);
+            if(str.charAt(i) == C.charAt(0)){
+                ck = true;
+                continue;
+            }else{
+                System.out.print(str.charAt(i));
+                
+            }
+            
         }
 
     }
--- String_ClassBasic/SubMatch1.java
+++ String_ClassBasic/SubMatch1.java
@@ -1,5 +1,17 @@
 package String_ClassBasic;
 
+import java.util.Scanner;
+
 public class SubMatch1 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String check = sc.next();   
+        for(int i=0; i<check.length()-2; i++){
+            String c = check.substring(i, i+3);
+            if(str.indexOf(c) != -1){
+                System.out.println(str.indexOf(c)+1);
+            }else System.out.println("No"); 
+        }
+    }
 }
