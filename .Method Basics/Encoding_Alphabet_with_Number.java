d954af2794d4f4837153b995a02c2e0336c4aec8
halinchanok <116154769+halinchanok@users.noreply.github.com>
2023-01-25 21:09:19 +0700

    Finish

---
+++ .Method Basics/Encoding_Alphabet_with_Number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Encoding_Alphabet_with_Number {
+    public static void taxt(int n, int i, String str, char num) {
+        if (num > 90) {
+            char c = (char) (str.charAt(i) + n - 26);
+            System.out.print(c);
+        } else {
+            char c = (char) (str.charAt(i) + n);
+            System.out.print(c);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        for (int i = 0; i < str.length(); i++) {
+            int n = sc.nextInt();
+            char num = (char) (str.charAt(i) + n);
+            taxt(n, i, str, num);
+        }
+
+    }
+}
