3719b4fcc16e492a54f2b229605e158ad468df12
halinchanok <116154769+halinchanok@users.noreply.github.com>
2023-04-25 22:34:30 +0700

    Finish

---
+++ BloodMoney.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BloodMoney {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            sc.nextLine();
+            String str = sc.nextLine();
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '#') {
+
+                } else if (str.charAt(j) == '@') {
+                    System.out.print(" ");
+                } else if (str.charAt(j) == '$') {
+                    System.out.print("[Blood Money ");
+                } else {
+                    System.out.print(str.charAt(j));
+                }
+            }
+            System.out.println("]");
+        }
+    }
+}
